d2abe08bd22d66ab9773ab9f8820027ec714f083
Najdorf12 <agustin.morro@gmail.com>
2024-09-19 14:29:36 -0300

    add gsap and Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,14 +8,15 @@ const Home = ({ allEvas }) => {
   return (
     <main className="bg-zinc-800">
         <section className="w-full h-[100dvh] flex flex-col items-center">
-          <nav className=" w-full flex  justify-between items-center pr-3">   
+          <nav className=" w-full flex  justify-between items-center pr-1 mt-2">   
             <div>
               <img src={imgLogo} alt="logo" 
               className="w-16"
               />
             </div>
+            <button className="px-5 text-base rounded-full border border-zinc-600  font-text2 text-whiteCustom">Contacto</button>
           </nav>
-          <article className="self-start pl-3 flex flex-col items-start mt-3">
+          <article className="self-start pl-3 flex flex-col items-start mt-4">
           <h2 className="font-title text-zinc-700 text-7xl sm:text-8xl">Evas del Eden</h2>
           <h3 className="text-whiteCustom text-2xl font-text2">Escorts Mendoza</h3>
           </article>
