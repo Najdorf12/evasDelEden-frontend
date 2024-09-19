6201a7662c623d5c3bf76e6d645e27c0476caf5f
Najdorf12 <agustin.morro@gmail.com>
2024-09-19 00:48:55 -0300

    fix intro

--- src/components/AgeVerification.jsx
+++ src/components/AgeVerification.jsx
@@ -1,6 +1,8 @@
 // src/components/AgeVerification.js
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Footer from "./Footer";
+import imgBg from "../assets/bg/bg1.jpg";
 
 const AgeVerification = ({ onConfirm }) => {
   const navigate = useNavigate();
@@ -16,16 +18,35 @@ const AgeVerification = ({ onConfirm }) => {
   };
 
   return (
-    <section className="w-full h-screen bg-zinc-800 flex flex-col items-center pt-24">
-      <article className="flex flex-col justify-center items-center gap-3">
-        <h6 className="text-xl text-zinc-500">Verificación de Edad</h6>
-        <p className="text-zinc-700 text-sm text-center text-balance">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro explicabo est quam, omnis tenetur repellendus.</p>
-        <p className="text-base text-zinc-600">¿Eres mayor de edad?</p>
-        <div className="flex justify-evenly items-center gap-3 text-lg mt-3"> 
-          <button className="rounded-full px-6 border border-stone-600 text-zinc-600" onClick={handleYes}>Sí, soy mayor.</button>
-          <button className="rounded-full px-6 border border-stone-600 text-zinc-600" onClick={handleNo}>No, soy menor.</button>
-        </div>
-      </article>
+    <section className="relative flex flex-col items-center pt-32 w-full h-screen overflow-hidden">
+      
+      <section id="verification" className="w-full h-screen absolute inset-0 ">
+      </section>
+        <article id="box-glass" className="flex flex-col justify-center font-text items-center gap-3 z-50 py-6 px-3 w-[95%] rounded-3xl">
+          <h6 className="text-2xl text-stone-700">Verificación de Edad</h6>
+          <p className="text-white text-sm text-center text-balance">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro
+            explicabo est quam, omnis tenetur repellendus.
+          </p>
+          <p className="text-lg text-stone-700">¿Eres mayor de edad?</p>
+          <div className="flex justify-evenly items-center gap-3 text-base mt-3">
+            <button
+              className="rounded-full px-6 border border-stone-600 text-white bg-zinc-700"
+              onClick={handleYes}
+            >
+              Sí, soy mayor.
+            </button>
+            <button
+              className="rounded-full px-6 border border-stone-600 text-stone-800 "
+              onClick={handleNo}
+            >
+              No, soy menor.
+            </button>
+          </div>
+        </article>
+        <footer className="absolute bottom-1">
+          <Footer />
+        </footer>
     </section>
   );
 };
