3a0667a924fd7d053f355118813a34f8c8e16856
Najdorf12 <agustin.morro@gmail.com>
2024-09-19 21:31:39 -0300

    add styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,16 @@ import imgLogo from "../assets/logo-removebg.png";
 import Footer from "../components/Footer";
 
 const Home = ({ allEvas }) => {
+  const [selectedMenu, setSelectedMenu] = useState("Categorias");
   const [selectedCategory, setSelectedCategory] = useState("Todas");
+  const [selectedLocation, setSelectedLocation] = useState("Mendoza");
   const categories = ["Todas", "Platinum", "Gold", "Silver"];
+  const locations = ["Mendoza", "Córdoba", "Santa Fe", "Buenos Aires"];
+
   return (
     <main className="bg-zinc-800 relative overflow-hidden">
-     {/*  <div className="absolute -right-[50%] top-[60%] w-[110%] h-[1px] bg-primary rotate-[130deg]"></div>
-      <div className="absolute -right-[50%] top-[60%] w-[70%] h-[1px] bg-primary rotate-[130deg]"></div> */}
       <section className="w-full h-[100dvh] flex flex-col items-center">
-        <nav className=" w-full flex justify-between items-center pr-1 mt-2">
+        <nav className="w-full flex justify-between items-center pr-1 mt-2">
           <div>
             <img src={imgLogo} alt="logo" className="w-16" />
           </div>
@@ -18,36 +20,75 @@ const Home = ({ allEvas }) => {
             Contacto
           </button>
         </nav>
+
         <article className="self-start pl-3 flex flex-col items-start mt-4">
-          <h2 className="font-title text-zinc-700 text-7xl ">Evas del Eden</h2>
+          <h2 className="font-title text-zinc-700 text-7xl">Evas del Eden</h2>
           <h3 className="text-whiteCustom text-2xl font-text2">
             Escorts - Mendoza
           </h3>
         </article>
+
         <ul className="flex gap-6 text-base font-text2 text-zinc-500 mt-6 self-start">
           <li className="relative flex flex-col justify-start items-start">
-            <div className="relative pr-3 pb-[2px] border-b border-zinc-600 flex items-center ml-8">
+            <div
+              onClick={(e) => {
+                setSelectedMenu("Categorias");
+              }}
+              className={`relative flex items-center ml-8 cursor-pointer ${
+                selectedMenu === "Categorias" ? "text-primary" : "text-zinc-500"
+              }`}
+            >
               <i className="bx bxs-doughnut-chart text-lg absolute -left-6"></i>
               Categorias
             </div>
-            <div className="flex gap-4 w-full pl-3">
-              {categories.map((category) => (
-                <button
-                  key={category}
-                  onClick={() => setSelectedCategory(category)}
-                  className={`mt-3 rounded-full px-4 text-sm ${
-                    selectedCategory === category
-                      ? "bg-zinc-700 text-primary"
-                      : "border border-zinc-700"
-                  }`}
-                >
-                  {category}
-                </button>
-              ))}
+          </li>
+          <li className="relative flex flex-col justify-start items-start">
+            <div
+              onClick={() => setSelectedMenu("Ubicacion")}
+              className={`relative  flex items-center ml-8 cursor-pointer ${
+                selectedMenu === "Ubicacion" ? "text-primary" : "text-zinc-500"
+              }`}
+            >
+              <i className="bx bxs-map text-lg absolute -left-6"></i>
+              Ubicacion
             </div>
           </li>
         </ul>
+        {selectedMenu === "Categorias" ? (
+          <div className="flex gap-4 w-full pl-3">
+            {categories.map((category) => (
+              <button
+                key={category}
+                onClick={() => setSelectedCategory(category)}
+                className={` mt-3 rounded-full px-4 py-[1px] text-sm ${
+                  selectedCategory === category
+                    ? "bg-zinc-700 text-primary"
+                    : "border border-zinc-700 text-zinc-500"
+                }`}
+              >
+                {category}
+              </button>
+            ))}
+          </div>
+        ) : (
+          <div className="flex gap-4 w-full pl-3">
+            {locations.map((location) => (
+              <button
+                key={location}
+                onClick={() => setSelectedLocation(location)}
+                className={` mt-3 rounded-full px-[9px] py-[1px] text-sm ${
+                  selectedLocation === location
+                    ? "bg-zinc-700 text-primary"
+                    : "border border-zinc-700 text-zinc-500"
+                }`}
+              >
+                {location}
+              </button>
+            ))}
+          </div>
+        )}
       </section>
+
       <footer className="absolute bottom-0 text-zinc-700 flex justify-center font-medium w-full">
         <Footer />
       </footer>
