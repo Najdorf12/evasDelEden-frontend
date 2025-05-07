adae743741c872efd3f376a4fddeea9949de34d9
Najdorf <agustin.morro@gmail.com>
2025-05-07 12:07:08 -0300

    fix cloudflare service

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,6 +3,11 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://evas-del-eden-backend.vercel.app/api",
   withCredentials: "true",
+  maxContentLength: 100 * 1024 * 1024, // 100MB
+  maxBodyLength: 100 * 1024 * 1024, // 100MB
+  headers: {
+    'Content-Type': 'multipart/form-data'
+  }
 });
 export default instance;
 
@@ -10,4 +15,3 @@ export default instance;
 http://localhost:1212/api
 https://evas-del-eden-backend.vercel.app/api
 */
-
